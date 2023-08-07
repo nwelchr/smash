faa14a6ff68efd2938a65f9de0dd7a20e34c1bce
Nick Welch <nwelchr@gmail.com>
2023-08-07 13:44:49 -0700

    Add notes section

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -6,6 +6,29 @@ const Button = styled.button`
   height: 20px;
 `;
 
+const NotesForm = ({ charId, closeModal }) => {
+  const [notes, setNotes] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(notes);
+    setNotes("");
+    closeModal();
+  };
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="notes">Notes</label>
+        <input
+          value={notes}
+          onChange={(e) => setNotes(e.target.value)}
+          placeholder="Notes"
+        />
+        <button type="submit">Save</button>
+      </form>
+    </div>
+  );
+};
+
 const Notes = ({ data }) => {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const ref = useRef(null);
@@ -39,6 +62,7 @@ const Notes = ({ data }) => {
       <Button onClick={openModal}>Notes</Button>
       <dialog ref={ref} open={isModalOpen}>
         <button onClick={closeModal}>Close</button>
+        <NotesForm charId={data.id} closeModal={closeModal} />
       </dialog>
     </>
   );
